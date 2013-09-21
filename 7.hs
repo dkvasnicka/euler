35702b5db8ffd710526ab021d4d4ebd5ef809613
Daniel Kvasnicka <daniel.kvasnicka@me.com>
2013-09-21 18:15:02 +0200

    Problem 7

---
+++ 7.hs
@@ -0,0 +1,17 @@
+-- A fn to find out the boundaries for nth prime number
+primeBounds :: Int -> [Int]
+primeBounds n = 
+    map (\ x -> floor (x * fromIntegral n)) [lowerBoundary, upperBoundary]
+        where
+            upperBoundary = (log . fromIntegral) n + (log . log . fromIntegral) n
+            lowerBoundary = upperBoundary - 1
+
+
+-- Sieve of Eratosthenes
+sieve :: [Int] -> [Int] -> Int -> [Int]
+sieve (n:ns) d l = if n < l then sieve [x | x <- ns, mod x n /= 0] (d ++ [n]) l else d ++ [n] ++ ns
+
+problem7 :: Int -> [Int]
+problem7 n = sieve [2..n] [] ((floor . sqrt . fromIntegral) n)    
+
+main = print ((problem7 (last (primeBounds 10001))) !! 10000)
