cc461ebab9eb249770d6ae26b770897ec5620032
Daniel Kvasnicka <daniel.kvasnicka@me.com>
2013-09-21 18:21:49 +0200

    Cleanup - parens don't look nice in haskell

--- 7.hs
+++ 7.hs
@@ -1,7 +1,7 @@
 -- A fn to find out the boundaries for nth prime number
 primeBounds :: Int -> [Int]
 primeBounds n = 
-    map (\ x -> floor (x * fromIntegral n)) [lowerBoundary, upperBoundary]
+    map (\ x -> floor $ x * fromIntegral n) [lowerBoundary, upperBoundary]
         where
             upperBoundary = (log . fromIntegral) n + (log . log . fromIntegral) n
             lowerBoundary = upperBoundary - 1
@@ -12,6 +12,6 @@ sieve :: [Int] -> [Int] -> Int -> [Int]
 sieve (n:ns) d l = if n < l then sieve [x | x <- ns, mod x n /= 0] (d ++ [n]) l else d ++ [n] ++ ns
 
 problem7 :: Int -> [Int]
-problem7 n = sieve [2..n] [] ((floor . sqrt . fromIntegral) n)    
+problem7 n = sieve [2..n] [] (floor . sqrt . fromIntegral $ n)    
 
-main = print ((problem7 (last (primeBounds 10001))) !! 10000)
+main = print $ (problem7 (last $ primeBounds 10001)) !! 10000
