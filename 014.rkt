48382f7b48c71d5d35b0347bf527dce180d51770
Daniel Kvasnicka <daniel.kvasnicka@me.com>
2014-04-04 00:16:15 +0200

    Initial (slow) version of P14

---
+++ 014.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+(define/match [populate n]
+  [[(? odd?)]  (+ 1 (* 3 n))]
+  [[(? even?)] (/ n 2)])
+
+(define [collatz-length n]
+  (letrec [[inner 
+            (λ [n cnt]
+               (if (eq? 1 n)
+                   cnt
+                   (inner (populate n) (+ 1 cnt))))]]
+    (inner n 1)))
+
+(define [p14 n maxlen result]
+  (if (eq? 1 n)
+      result
+      (let* [[curlen (collatz-length n)]
+             [better? (> curlen maxlen)]]
+        (p14 (- n 1)
+             (if better? curlen maxlen)
+             (if better? n result)))))
+
+(time (p14 999999 0 0))
