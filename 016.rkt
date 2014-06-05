ab15ddc2c91d24777682e386b3822fa50b3692cd
Daniel Kvasnicka <daniel.kvasnicka@me.com>
2014-06-06 00:19:47 +0200

    Initial version of Problem 16 - bugged

---
+++ 016.rkt
@@ -0,0 +1,26 @@
+#lang typed/racket/base
+
+[: number->digits (Integer -> (Listof Integer))]
+
+(define [number->digits n] 
+  (if (< n 10)
+      (list n)
+      (cons (remainder n 10) (number->digits (quotient n 10)))))
+
+[: sum-doubled-digits (Integer -> Integer)]
+
+(define [sum-doubled-digits n]
+  (foldl (lambda: [(d : Integer) (accum : Integer)]
+                  (+ accum 
+                     (let-values [[[l r] (quotient/remainder (* 2 d) 10)]]
+                                 (+ l r)))) 
+         0 (number->digits n)))
+
+[: power-of-two-digit-sum (Integer -> Integer)]
+
+(define [power-of-two-digit-sum e]
+  (if (= e 1)
+      2
+      (sum-doubled-digits (power-of-two-digit-sum (sub1 e)))))
+
+(power-of-two-digit-sum 11)
