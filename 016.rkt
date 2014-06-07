e20a19ef91d34c3d02a7b20d5406fdd7ddd6abb5
Daniel Kvasnicka <daniel.kvasnicka@me.com>
2014-06-07 22:24:15 +0200

    Problem 16 - actually solved

--- 016.rkt
+++ 016.rkt
@@ -1,7 +1,37 @@
 #lang typed/racket/base
 
-(require math/base)
+(require math/base
+         racket/list)
 
+; A version with custom-written multiplication. Slow as hell because 'append'
+; on lists goes in linear time.
+; Would be fast using (mutable?) constant-time access vectors.
+(: *2 ((Listof Integer) -> (Listof Integer)))
+
+(define [*2 n]
+  (let: loop [[result : (Listof Integer) '()]
+              [num n]
+              [carryover 0]]
+    (if (and (empty? num) (= carryover 0))
+        result
+        (if (empty? num)
+          (append result (list carryover))
+          (let-values [[[q r] (quotient/remainder (* 2 (car num)) 10)]]
+                      (loop
+                        (append result (list (+ carryover r)))
+                        (cdr num)
+                        q))))))
+
+(: two-to-the-power-of (Integer -> (Listof Integer)))
+
+(define [two-to-the-power-of e]
+  (if (= e 1)
+      '(2)
+      (*2 (two-to-the-power-of (sub1 e)))))
+
+(time (sum (two-to-the-power-of 1000)))
+
+; Freaking fast version using built-in expt function. Can go in less than 1 ms on a 3 yrs old MBP
 [: number->digits (Integer -> (Listof Integer))]
 
 (define [number->digits n] 
@@ -9,23 +39,4 @@
       (list n)
       (cons (remainder n 10) (number->digits (quotient n 10)))))
 
-[: sum-doubled-digits (Integer -> Integer)]
-
-(define [sum-doubled-digits n]
-  (foldl (lambda: [(d : Integer) (accum : Integer)]
-                  (displayln d)
-                  (+ accum 
-                     (let-values [[[l r] (quotient/remainder (* 2 d) 10)]]
-                                 (+ l r)))) 
-         0 (number->digits n)))
-
-[: power-of-two-digit-sum (Integer -> Integer)]
-
-(define [power-of-two-digit-sum e]
-  (if (= e 1)
-      2
-      (sum-doubled-digits (power-of-two-digit-sum (sub1 e)))))
-
-(time (power-of-two-digit-sum 6))
-
-; double just the number; sum in the end
+(time (sum (number->digits (expt 2 1000))))
