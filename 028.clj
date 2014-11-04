5eb62fe8250acbf5bc98cde568e1cafd5ebf39b7
Daniel Kvasnicka <daniel.kvasnicka@me.com>
2014-11-04 22:43:27 +0100

    Problem 28 - fully lazy

--- 028.clj
+++ 028.clj
@@ -1,8 +1,6 @@
 
 (reduce +
-        (reduce
-          (fn [accum in] (cons (+ in (first accum)) accum))
-          '(1)
+        (reductions + 1
           (mapcat 
             (partial repeat 4) 
             (filter even? (range 2 1001)))))
